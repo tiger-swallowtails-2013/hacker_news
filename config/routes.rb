72d964db064a8721e3c53eb6a8523edbfd5ecbd4
Apprentice <apprentice@dbc30.local>
2013-10-07 14:33:07 -0700

    add views for user and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 HackerNews::Application.routes.draw do
-  # get '/users'
+   get '/home/index', to: "home#index"
+   get '/users/new', to: "users#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
