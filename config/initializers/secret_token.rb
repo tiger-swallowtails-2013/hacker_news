e2e3366760e7f8f68900ac01a57908279923ab87
Apprentice <apprentice@dbc30.local>
2013-10-07 12:32:01 -0700

    create skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNews::Application.config.secret_key_base = '9fe5cbd2cf5a66037d72ceb3b59b54410df2a11394f89e11b814307c8d3266b28627a7ae3eefccc2a8eebdb7e759021fcac35897abe93bccd03439db3b2753e2'
