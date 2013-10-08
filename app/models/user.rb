09e76239785b4e826541d6eee1caee6c369eae20
Apprentice <apprentice@dbc30.local>
2013-10-07 19:45:23 -0700

    add validations for email and password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
+
 end  
