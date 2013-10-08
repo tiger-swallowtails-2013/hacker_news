0e5c3255f05a8f8228410ba07d22feb96b663023
Apprentice <apprentice@dbc-24.local>
2013-10-08 16:47:30 -0700

    test for post-creation errors in view

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 require 'capybara/rspec'
 
 feature "Post creation" do
-  scenario 'with title and body' do
+  scenario 'with correct title and body' do
     visit new_post_path
     fill_in 'post[title]', with: 'My Post'
     fill_in 'post[body]', with: 'Here I am.'
-    click_button 'Save Post'
+    click_button 'Create Post'
     expect(page).to have_content('My Post')
+  end
+  scenario 'with invalid title' do
+    visit new_post_path
+    fill_in 'post[title]', with: 'bad'
+    fill_in 'post[body]', with: 'A complete sentence.'
+    click_button 'Create Post'
+    expect(page).to have_content("too short")
   end
 end
