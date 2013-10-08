12c4795d11ae9e0c0b040c284c2e543d7750e744
Apprentice <apprentice@dbc-21.local>
2013-10-08 15:56:36 -0700

    update post model to incorporate self-referential posts (aka comments)

--- db/migrate/20131007211424_create_posts.rb
+++ db/migrate/20131007211424_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :posts
+      t.integer :parent_post_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20131007211424) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "posts_id"
+    t.integer  "parent_post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
