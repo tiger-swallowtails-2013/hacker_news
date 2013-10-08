6516b8c9117c34fb312b69c3161ad6271e947d57
Apprentice <apprentice@dbc-21.local>
2013-10-08 14:44:35 -0700

    add to post model a self-reference, remigrated database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments, :class_name => "Post",
+    :foreign_key => "parent_post_id"
+  belongs_to :parent_post, :class_name => "Post"
   validates :title, presence: true
   validates :body, presence: true
 end
