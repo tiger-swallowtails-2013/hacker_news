f7626926597ecc0b77b940a90180074c672a12ba
Apprentice <apprentice@dbc-21.local>
2013-10-08 15:55:54 -0700

    add partials for post views based on comment or post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    if params[:yield] == "comment"
+      @view = "comment"
+    else
+      @view = "post"
+    end
+  end
+
   def create
-    Post.create(params[:post].permit(:title, :body))
+    Post.create(params[:post].permit(:title, :body, :parent_post_id))
     redirect_to posts_path
   end
 end
