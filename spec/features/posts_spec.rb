fc0e0ec86254c85e6f60ecec11e5a83a53b3b303
Apprentice <apprentice@dbc-21.local>
2013-10-08 16:25:55 -0700

    add feature testing for comments and posts, succeeding

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 require 'capybara/rspec'
 
-feature "Post creation" do
+feature "post creation" do
   scenario 'with title and body' do
-    visit new_post_path
+    visit new_post_path(:yield => "post")
     fill_in 'post[title]', with: 'My Post'
     fill_in 'post[body]', with: 'Here I am.'
     click_button 'Save Post'
     expect(page).to have_content('My Post')
+  end
+  scenario 'if the post is a comment' do
+    visit new_post_path(:yield => "comment")
+    fill_in 'post[title]', with: 'My Post'
+    fill_in 'post[body]', with: 'Here I am.'
+    click_button 'Save Comment'
+    expect(page).to have_content("My Post")
   end
 end
